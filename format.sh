4dff42468be47a2257162aa849b8e5e017944fff
Aidan Welch <aidan@freedwave.com>
2024-01-12 12:46:40 -0800

    update linting scripts

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -path ./build -prune -iname '*.h' -o -iname '*.cpp' | xargs clang-format-18 -style=file -i
+find . -path ./build -prune -iname '*.h' -o -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format-18 -style=file -i
--- tidy.sh
+++ tidy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . ./prebuild.sh "$1"
-find ../src/ -iname '*.h' -o -iname '*.cpp' | xargs clang-tidy-18 -p="build" --config-file="../.clang-tidy" -extra-arg=-std=c++20
+find ../ -path ./build -prune -o -iname '*.hpp' -o -iname '*.cpp' | xargs clang-tidy-18 -p="build" --config-file="../.clang-tidy" -extra-arg=-std=c++20
