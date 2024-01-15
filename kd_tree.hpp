6a86b50376d82a66795d8937c29ff91cd37ecfdf
Aidan Welch <aidan@freedwave.com>
2024-01-15 17:07:41 +0100

    update primary kd_tree file

--- kd_tree.hpp
+++ kd_tree.hpp
@@ -32,7 +32,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 ///////////////////////////////////////////////////////////////////////////////
 
-
 #ifndef SPATIAL_LIB_KD_TREE_HPP_
 #define SPATIAL_LIB_KD_TREE_HPP_
 
@@ -40,46 +39,104 @@ SOFTWARE. */
 #include <cmath>
 #include <concepts>
 #include <cstddef>
-#include <cstdint>
+#include <tuple>
 #include <type_traits>
 #include <vector>
+#include <execution>
 
 namespace spatial_lib {
 
-// This constraint requires that coordinates is a vector
-template <typename T> concept KDTreeVectorDataConstraint = requires( T t ) {
-	requires std::same_as<
-		decltype( t.coordinates ),
-		std::vector<typename decltype( t.coordinates )::value_type>>;
+namespace kd_tree_types {
+
+template <typename Container> concept IsContainer = requires( Container container ) {
+	{ container.begin() } -> std::same_as<typename Container::iterator>;
+	{ container.end() } -> std::same_as<typename Container::iterator>;
+} || std::is_array_v<Container>;
+
+template <typename T> concept IsValidCoordinates =
+	IsContainer<T> && requires( T a, T b, std::size_t index ) {
+		{ a[index] < b[index] } -> std::convertible_to<bool>;
+	};
+
+template <typename T> concept IsValidDataType = IsValidCoordinates<decltype( T::coordinates )>;
+
+template <typename Container> concept InputContainsValidDataTypeNotCArray =
+	IsValidDataType<typename Container::value_type>;
+
+template <typename Container> concept InputCArrayContainsValidDataType =
+	std::is_array_v<Container> && IsValidDataType<std::remove_all_extents_t<Container>>;
+
+template <typename Container> concept IsDynamicContainer = requires( Container container ) {
+	{ container.push_back( Container::value_type ) } -> std::same_as<void>;
 };
 
-template <typename T> concept KDTreeArrayDataConstraint =
-	requires( T t ) { requires std::is_array_v<decltype( t.coordinates )>; };
+template <typename Container> concept IsValidInput = IsContainer<Container> &&
+	( InputCArrayContainsValidDataType<Container> ||
+	  InputContainsValidDataTypeNotCArray<Container> );
 
-template <typename T>
-	requires KDTreeVectorDataConstraint<T> || KDTreeArrayDataConstraint<T>
-class KD_Tree_Base {
-	protected:
-	using CoordinatesType = decltype( T::coordinates );
+template <typename Container> concept IsStaticContainer =
+	IsContainer<Container> && ( !IsDynamicContainer<Container> );
+template <typename Container> concept IsCArray = std::is_array_v<Container>;
+template <typename Container> concept IsStaticContainerNotCArray =
+	IsStaticContainer<Container> && ( !IsCArray<Container> );
+
+template <typename T> constexpr const void* staticSize = nullptr;
+
+template <IsStaticContainerNotCArray T> constexpr std::size_t staticSize<T> = std::tuple_size_v<T>;
+
+template <IsCArray T> constexpr std::size_t staticSize<T> = std::extent_v<T>;
+
+template <typename Container> concept InputContainsStaticCoordinates =
+	(IsCArray<Container> &&
+	 IsStaticContainer<decltype( std::remove_all_extents_t<Container>::coordinates )>) ||
+	IsStaticContainer<decltype( Container::value_type::coordinates )>;
+
+template <typename T> constexpr const void* staticDimensions = nullptr;
+
+template <typename Container> concept InputCArrayContainsStaticCoordinates =
+	InputCArrayContainsValidDataType<Container> && InputContainsStaticCoordinates<Container>;
+template <typename Container> concept InputContainsStaticCoordinatesNotCArray =
+	InputContainsValidDataTypeNotCArray<Container> && InputContainsStaticCoordinates<Container>;
+
+template <InputCArrayContainsStaticCoordinates T>
+constexpr std::size_t staticDimensions<T> =
+	staticSize<decltype( std::remove_all_extents_t<T>::coordinates )>;
+
+template <InputContainsStaticCoordinatesNotCArray T>
+constexpr std::size_t staticDimensions<T> = staticSize<decltype( T::value_type::coordinates )>;
+
+}  // namespace kd_tree_types
+
+template <kd_tree_types::IsValidInput Input> class KD_Tree {
+
+	Input input_data;
+
+	using DataType = std::conditional_t<
+		std::is_array_v<Input>,
+		std::remove_all_extents_t<Input>,
+		typename Input::value_type>;
 
 	struct Node {
 		Node* left;
 		Node* right;
-		T* data;
+		DataType* data;
 	};
 
-	std::vector<Node> nodes; // NOLINT misc-non-private-member-variables-in-classes
-	Node* root = nullptr; // NOLINT misc-non-private-member-variables-in-classes
+	using PresortedContainer = std::conditional_t<
+		kd_tree_types::InputContainsStaticCoordinates<Input>,
+		std::array<std::vector<Node*>, kd_tree_types::staticDimensions<Input>>,
+		std::vector<std::vector<Node*>>>;
 
-	virtual Node* get_node_from_presorted_dimensions( std::size_t depth, std::size_t index ) = 0;
-	virtual void
-		presort_dimensions_and_push_nodes( std::vector<T>* data_vector, std::size_t data_size ) = 0;
+	std::vector<Node> nodes;
+
+	Node* root = nullptr;
+
+	std::size_t dimensions = 0;
+
+	PresortedContainer presorted_dimensions;
 
 	void link_tree(
-		const std::size_t start,
-		const std::size_t end,
-		const std::size_t depth,
-		Node*& tree_place // I believe this reference slightly helps with tail call optimization on the last layer
+		const std::size_t start, const std::size_t end, const std::size_t depth, Node*& tree_place
 	) {
 
 		if ( start == end ) {
@@ -94,19 +151,84 @@ class KD_Tree_Base {
 		link_tree( midpoint + 1, end, depth + 1, tree_place->right );
 	}
 
+	inline void presort_dimension( std::size_t dim ) {
+		std::sort(
+			std::execution::par_unseq,
+			presorted_dimensions[dim].begin(),
+			presorted_dimensions[dim].end(),
+			[dim]( const Node* node1, const Node* node2 ) {
+				return node1->data->coordinates[dim] < node2->data->coordinates[dim];
+			}
+		);
+	}
+
 	public:
-	explicit KD_Tree_Base( std::vector<T>& data_vector ){};
+	/// Only pass a pointer to the KD Tree if you're sure that input_data will be preserved
+	/// in scope for the lifetime of the KD Tree.
+	explicit KD_Tree<Input>( Input* input_data ) { generate_tree( input_data ); };
 
-	void balance_tree( std::vector<T>& data_vector ) { balance_tree( &data_vector ); }
+	/// Passing by value leads to the value being moved, this should only be done to preserve
+	/// the input_data if it would otherwise go out of scope.
+	explicit KD_Tree<Input>( Input input_data ) : input_data( std::move( input_data ) ) {
+		generate_tree( &( this->input_data ) );
+	}
 
-	void balance_tree( std::vector<T>* data_vector = nullptr ) {
-		std::size_t total_size =
-			( data_vector == nullptr ) ? nodes.size() : nodes.size() + data_vector->size();
+	void generate_tree( Input* data_container = nullptr ) {
+		if constexpr ( kd_tree_types::InputContainsStaticCoordinates<Input> ) {
+			dimensions = kd_tree_types::staticDimensions<Input>;
+		} else if ( data_container != nullptr && !data_container->empty() ) {
+			dimensions = ( *data_container )[0].coordinates.size();
+		}
+		std::size_t total_size = nodes.size();
+		if constexpr ( std::is_array_v<Input> ) {
+			total_size += std::extent_v<Input>;
+		} else {
+			total_size += data_container->size();
+		}
 
 		// reserve the space for all the data in the presorted dimensions
 		nodes.reserve( total_size );
-		presort_dimensions_and_push_nodes( data_vector, total_size );
-		link_tree(0, total_size, 0, root);
+		if constexpr ( kd_tree_types::InputContainsStaticCoordinates<Input> ) {
+			for ( std::size_t i = 0; i < kd_tree_types::staticDimensions<Input>; i++ ) {
+				presorted_dimensions[i].reserve( total_size );
+			}
+		} else {
+			// reserve a vector for every dimension
+			presorted_dimensions.reserve( dimensions );
+			for ( std::size_t i = 0; i < dimensions; i++ ) {
+				presorted_dimensions.emplace_back();
+				presorted_dimensions[i].reserve( total_size );
+			}
+		}
+
+		// add existing nodes to the presorted vectors
+		for ( Node& node : nodes ) {
+			for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
+				presorted_dim.emplace_back( &node );
+			}
+		}
+
+		// add the new nodes in the data vector to both nodes and presorteds
+		if ( data_container != nullptr ) {
+			for ( DataType& data : *data_container ) {
+				nodes.emplace_back( nullptr, nullptr, &data );
+				for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
+					presorted_dim.emplace_back( &nodes.back() );
+				}
+			}
+		}
+
+		// actually sort the presorted dimensions
+		if constexpr ( kd_tree_types::InputContainsStaticCoordinates<Input> ) {
+			for ( std::size_t dim = 0; dim < kd_tree_types::staticDimensions<Input>; dim++ ) {
+				presort_dimension( dim );
+			}
+		} else {
+			for ( std::size_t dim = 0; dim < dimensions; dim++ ) {
+				presort_dimension( dim );
+			}
+		}
+		link_tree( 0, total_size, 0, root );
 	}
 
 	Node* nearest_neighbor( /* const std::vector<CoordinatesType>& coordinates */ ) {
@@ -115,149 +237,16 @@ class KD_Tree_Base {
 		}
 		return root;
 	}
-};
 
-template <typename T>
-	requires KDTreeVectorDataConstraint<T> || KDTreeArrayDataConstraint<T>
-class KD_Tree {};
-
-template <KDTreeVectorDataConstraint T> class KD_Tree<T> : public KD_Tree_Base<T> {
-	using CoordinatesType = KD_Tree_Base<T>::CoordinatesType;
-	using Node = KD_Tree_Base<T>::Node;
-	using KD_Tree_Base<T>::nodes;
-
-	std::size_t dimensions = 0;
-
-	std::vector<std::vector<Node*>> presorted_dimensions;
-
-	inline Node* get_node_from_presorted_dimensions( std::size_t depth, std::size_t index ) final {
-		return presorted_dimensions[depth % dimensions][index];
-	}
-
-	inline void presort_dimensions_and_push_nodes(
-		std::vector<T>* data_vector, std::size_t data_size
-	) final {
-		// reserve a vector for every dimension
-		presorted_dimensions.reserve( dimensions );
-		for ( std::size_t i = 0; i < dimensions; i++ ) {
-			presorted_dimensions.emplace_back();
-			presorted_dimensions[i].reserve( data_size );
+	inline Node* get_node_from_presorted_dimensions( std::size_t depth, std::size_t index ) {
+		if constexpr ( kd_tree_types::InputContainsStaticCoordinates<Input> ) {
+			return presorted_dimensions[depth % kd_tree_types::staticDimensions<Input>][index];
+		} else {
+			return presorted_dimensions[depth % dimensions][index];
 		}
-
-		// add existing nodes to the presorted vectors
-		for ( Node& node : nodes ) {
-			for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
-				presorted_dim.emplace_back( &node );
-			}
-		}
-
-		// add the new nodes in the data vector to both nodes and presorteds
-		if ( data_vector != nullptr ) {
-			for ( T& data : *data_vector ) {
-				nodes.emplace_back( nullptr, nullptr, &data );
-				for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
-					presorted_dim.emplace_back( &nodes.back() );
-				}
-			}
-		}
-
-		// actually sort the presorted dimensions
-		for ( std::size_t dim = 0; dim < dimensions; dim++ ) {
-			std::sort(
-				presorted_dimensions[dim].begin(),
-				presorted_dimensions[dim].end(),
-				[dim]( const Node* node1, const Node* node2 ) {
-					return node1->data->coordinates[dim] < node2->data->coordinates[dim];
-				}
-			);
-		}
-	}
-
-	public:
-	explicit KD_Tree( std::vector<T>& data_vector )
-		: KD_Tree_Base<T>( data_vector ), dimensions( data_vector[0].coordinates.size() ) {
-		KD_Tree_Base<T>::balance_tree( &data_vector );
-	};
-};
-
-template <KDTreeArrayDataConstraint T> class KD_Tree<T> : public KD_Tree_Base<T> {
-	using CoordinatesType = decltype( T::coordinates );
-	static constexpr std::size_t dimensions = std::extent_v<CoordinatesType>;
-	using Node = KD_Tree_Base<T>::Node;
-	using KD_Tree_Base<T>::nodes;
-
-	std::array<std::vector<Node*>, dimensions> presorted_dimensions;
-
-	inline Node* get_node_from_presorted_dimensions( std::size_t depth, std::size_t index ) final {
-		return presorted_dimensions[depth % dimensions][index];
-	}
-
-	// this can probably be mostly shared in base
-	inline void presort_dimensions_and_push_nodes(
-		std::vector<T>* data_vector, std::size_t data_size
-	) final {
-		// reserve the space for all the data in the presorted dimensions
-		for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
-			presorted_dim.reserve( data_size );
-		}
-
-		// add existing nodes to the presorted vectors
-		for ( Node& node : nodes ) {
-			for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
-				presorted_dim.emplace_back( &node );
-			}
-		}
-
-		// add the new nodes in the data vector to both nodes and presorteds
-		if ( data_vector != nullptr ) {
-			for ( T& data : *data_vector ) {
-				nodes.emplace_back( nullptr, nullptr, &data );
-				for ( std::vector<Node*>& presorted_dim : presorted_dimensions ) {
-					presorted_dim.emplace_back( &nodes.back() );
-				}
-			}
-		}
-
-		// actually sort the presorted dimensions
-		for ( std::size_t dim = 0; dim < dimensions; dim++ ) {
-			std::sort(
-				presorted_dimensions[dim].begin(),
-				presorted_dimensions[dim].end(),
-				[dim]( const Node* node1, const Node* node2 ) {
-					return node1->data->coordinates[dim] < node2->data->coordinates[dim];
-				}
-			);
-		}
-	}
-
-	public:
-	explicit KD_Tree( std::vector<T>& data_vector ) : KD_Tree_Base<T>( data_vector ) {
-		KD_Tree_Base<T>::balance_tree( &data_vector );
-	};
-
-	Node* link_not_presorted( std::int64_t start, std::int64_t end, const std::size_t depth ) {
-		if ( start == end ) {
-			return nullptr;
-		}
-
-		std::sort(
-			nodes.begin() + start,
-			nodes.begin() + end,
-			[depth]( const Node& node1, const Node& node2 ) {
-				return node1.data->coordinates[depth % dimensions] <
-					node2.data->coordinates[depth % dimensions];
-			}
-		);
-
-		const std::int64_t midpoint = start + ( ( end - start ) / 2 );
-		Node& median = nodes[static_cast<std::size_t>( midpoint )];
-
-		median.left = link_not_presorted( start, midpoint, depth + 1 );
-		median.right = link_not_presorted( midpoint + 1, end, depth + 1 );
-		return &median;
 	}
 };
 
-}  // namespace spatial_lib
+}  //  namespace spatial_lib
 
 #endif
